b06b01c4245dc3807d83b9a8ce4f6716e0fbea26
todonotcool-debug <todonotcool@gmail.com>
2026-04-30 20:41:42 +0530

    Update ContentView.swift

--- sketch-elements/ContentView.swift
+++ sketch-elements/ContentView.swift
@@ -1,71 +1,58 @@
-//
-//  ContentView.swift
-//  sketch-elements
-//
-//  Created by Filip Molcik on 12/03/2020.
-//  Copyright © 2020 Filip Molcik. All rights reserved.
-//
-
 import SwiftUI
 
-// NOTE: This is needed only to change the theme, feel free to remove it
-enum Theme: String, CaseIterable, Identifiable {
-    case food
-    case social
-    case travel
-    case music
-    case photography
-    var id: String { self.rawValue }
+struct ContentView: View {
+    @State private var balloons: [Balloon] = []
+
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.pink, .purple],
+                           startPoint: .top,
+                           endPoint: .bottom)
+                .ignoresSafeArea()
+
+            VStack {
+                Text("Happy Birthday Mom 💖")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding(.top, 60)
+
+                Spacer()
+            }
+
+            ForEach(balloons) { balloon in
+                Text(balloon.emoji)
+                    .font(.system(size: 40))
+                    .position(balloon.position)
+            }
+        }
+        .onAppear {
+            spawnBalloons()
+        }
+    }
+
+    func spawnBalloons() {
+        Timer.scheduledTimer(withTimeInterval: 0.4, repeats: true) { _ in
+            let x = CGFloat.random(in: 50...350)
+            let emojis = ["🎈", "🎉", "💖"]
+            let newBalloon = Balloon(
+                id: UUID(),
+                position: CGPoint(x: x, y: 800),
+                emoji: emojis.randomElement()!
+            )
+            balloons.append(newBalloon)
+
+            withAnimation(.linear(duration: 5)) {
+                if let index = balloons.firstIndex(where: { $0.id == newBalloon.id }) {
+                    balloons[index].position.y = -100
+                }
+            }
+        }
+    }
 }
 
-struct ContentView: View {
-    
-    @EnvironmentObject var userData: UserData
-    @EnvironmentObject var viewModel: MusicViewModel
-    // NOTE: This is needed only to change the theme, feel free to remove it
-    @State var selectedTheme = Theme.food
-    // NOTE END
-    
-    var body: some View {
-                
-        return ZStack {
-            
-
-            // NOTE: This is needed only to change the theme, feel free to remove the swicth and instead of it use invoke the view directly
-            // Food()
-            // Social()
-            switch (selectedTheme) {
-                case Theme.food:
-                    Food()
-                case Theme.social:
-                    Social()
-                case Theme.travel:
-                    Travel()
-                case Theme.photography:
-                    Photography()
-                case Theme.music:
-                    Music()
-            
-            }
-            // NOTE END
-
-            ModalAnchorView()
-        }
-        
-        // NOTE: This is needed only to change the theme, feel free to remove it
-        .actionSheet(isPresented: Binding.constant(true)) {
-            
-            ActionSheet(title: Text("Select a Theme"), message: Text("To invoke this menu again, you need to rerun the app. You can also set the theme directly in ContentView.swift"), buttons: [
-                .default(Text("Social")) { self.selectedTheme = .social },
-                .default(Text("Travel")) { self.selectedTheme = .travel },
-                .default(Text("Food")) { self.selectedTheme = .food },
-                .default(Text("Music")) { self.selectedTheme = .music },
-                .default(Text("Photography")) {self.selectedTheme = .photography},
-                .cancel()
-            ])
-        }
-        // NOTE END
-
-        
-    }
+struct Balloon: Identifiable {
+    let id: UUID
+    var position: CGPoint
+    var emoji: String
 }
